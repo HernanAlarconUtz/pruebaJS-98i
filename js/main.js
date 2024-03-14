493bcb0fe2e38cfc32dab8b142f7907da99a72b3
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-03-14 15:56:08 -0300

    primer commit de los ejercicios js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("Hola mundo!");
+
+let nombre = "Hernan";
+console.log(nombre);
+
+
+
+//alert("este es un texto en un alert, bienvenido " + nombre);
+
+
+const repuesta = prompt("ingrese su nombre");
+console.log(repuesta);
