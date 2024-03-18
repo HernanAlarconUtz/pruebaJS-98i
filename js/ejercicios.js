8b39981911c590bf79356ce98662a2408ddcfb6b
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-03-18 17:10:21 -0300

    Ejercicio 1, 2, 3

---
+++ js/ejercicios.js
@@ -0,0 +1,12 @@
+
+//1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+
+alert("un mensaje");
+
+//2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+
+document.write("Hola mundo","<br>");
+
+//3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+document.write("3+5 = ", 3+5,"<br>");
