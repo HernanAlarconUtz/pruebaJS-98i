f1a11b233bddc0306bfdaa17eb12e94db8e914ea
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-04-10 17:10:24 -0300

    Se agrego el ejercicio 6 del tp3

---
+++ js/tp3/ejercicio6.js
@@ -0,0 +1,31 @@
+// 6- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, luego crear una función para calcular su perímetro y mostrarlo por pantalla.
+
+// La fórmula del perímetro  es p = 2*(a +b)
+
+function calcularPerimetro(ladoA, ladoB) {
+  if (isNaN(ladoA) || ladoA <= 0) {
+    return "El valor del lado A no es un numero valido";
+  } else if (isNaN(ladoB) || ladoB <= 0) {
+    return "El valor del lado B no es un numero valido";
+  } else {
+    const perimetro = 2 * (ladoA + ladoB);
+    return perimetro;
+  }
+}
+
+let ladoA;
+let ladoB;
+
+do {
+  ladoA = parseInt(prompt("Ingrese el valor del lado A del rectangulo:"));
+  ladoB = parseInt(prompt("Ingrese el valor del lado B del rectangulo:"));
+} while (isNaN(ladoA) || isNaN(ladoB) || ladoA <= 0 || ladoB <= 0);
+
+let resultado = calcularPerimetro(ladoA, ladoB);
+if (typeof resultado === "string") {
+  alert(resultado);
+} else {
+  alert(
+    `El perimetro del rectangulo con lado A = ${ladoA} y lado B = ${ladoB}, es igual a  ${resultado}`
+  );
+}
