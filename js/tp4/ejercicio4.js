57f0f315881c68c69b9c7279f7ccf97e7a2a2b52
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-04-17 15:50:12 -0300

    Se añadio el ejercicio 4 del tp4

---
+++ js/tp4/ejercicio4.js
@@ -0,0 +1,29 @@
+// 4- Escribe una clase Producto para crear objetos. Estos objetos, deben presentar las propiedades código, nombre y precio, además del método imprime datos, el cual escribe por pantalla los valores de sus propiedades.
+// Posteriormente, cree tres instancias de este objeto y guárdalas en un array.
+// Por último, utilice el método imprime datos para mostrar por pantalla los valores de los tres objetos instanciados.
+
+const productos = [];
+
+class Producto {
+  constructor(codigo, nombre, precio) {
+    this.codigo = codigo;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+
+  mostrarDatos() {
+    document.write(`${this.codigo} ${this.nombre} ${this.precio} </br>`);
+  }
+}
+
+const objeto1 = new Producto(12, "Procesador", 1500000);
+const objeto2 = new Producto(13, "SSD", 15000);
+const objeto3 = new Producto(14, "Placa madre", 10000);
+
+productos.push(objeto1);
+productos.push(objeto2);
+productos.push(objeto3);
+
+productos.forEach((producto) => {
+    producto.mostrarDatos()
+  });
