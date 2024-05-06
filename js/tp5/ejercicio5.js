8e33b59940cd06e4e84b594ee1002e5b0a868fbd
Hernan Alarcon Utz <hernanalarcon51@gmail.com>
2024-05-05 22:57:33 -0300

    se corrigio  el flujo de trabajo de los botones y se agrego formato a los numeros del cronometro

--- js/tp5/ejercicio5.js
+++ js/tp5/ejercicio5.js
@@ -5,8 +5,8 @@ const $iniciar = document.getElementById("botonIniciar");
 const $reset = document.getElementById("botonReset");
 const $pausa = document.getElementById("botonPausa");
 
-$pausa.disabled = true;
-$reset.disabled = true;
+$reset.disabled = true
+$pausa.disabled = true
 
 let intervalo = null;
 let cronometroIniciado = false;
@@ -19,32 +19,48 @@ const cronometro = () => {
     const minutos = Math.floor((tiempo % 3600) / 60);
     const segundos = tiempo % 60;
 
-    $cronometroTexto.textContent = `${horas}:${minutos}:${segundos}`;
+    $cronometroTexto.textContent = `${formatoNumero(horas)}:${formatoNumero(minutos)}:${formatoNumero(segundos)}`;
   }, 1000);
 };
 
-const habilitarBotones = () => {
-  $pausa.disabled = false;
-  $reset.disabled = false;
-};
+const formatoNumero = (numero) => {
+    if (numero < 10) {
+        numero = "0" + numero;
+    }
+    return numero;
+}
 
-const deshabilitarBotones = () => {
-  $pausa.disabled = true;
-  
-};
+const habilitarIniciarReset = () => {
+    $iniciar.disabled = false;
+    $reset.disabled = false;
+    $pausa.disabled = true;
+  };
+
+  const habilitarPausaReset = () => {
+    $pausa.disabled = false;
+    $iniciar.disabled = true;
+    $reset.disabled = false;
+  };
+
+  const habilitarIniciar = () => {
+    $reset.disabled = true;
+    $iniciar.disabled = false;
+    $pausa.disabled = true;
+  };
+
 
 $iniciar.addEventListener("click", () => {
   if (!cronometroIniciado) {
     cronometro();
     cronometroIniciado = true;
-    habilitarBotones();
+    habilitarPausaReset()
   }
 });
 
 $pausa.addEventListener("click", () => {
   clearInterval(intervalo);
   cronometroIniciado = false;
-  deshabilitarBotones();
+  habilitarIniciarReset()
 });
 
 $reset.addEventListener("click", () => {
@@ -52,5 +68,5 @@ $reset.addEventListener("click", () => {
   tiempo = 0;
   $cronometroTexto.textContent = "00:00:00";
   cronometroIniciado = false;
-  deshabilitarBotones();
+  habilitarIniciar();
 });
